03001fb2358216c9be59665ffda952784f18c693
Leslie Bernhardt <mareenbern@gmail.com>
2023-01-25 12:58:31 +0100

    form draft 2

--- pages/form.tsx
+++ pages/form.tsx
@@ -3,13 +3,6 @@ import Image from 'next/image'
 import style from '../styles/layout.module.css'
 import React from 'react';
 
-const EndometriosisForm = () => {
-    const [selectedOption, setSelectedOption] = useState('');
-  
-    const handleOptionChange = (e) => {
-      setSelectedOption(e.target.value);
-    }
-  
 
 export default function Home() {
 	const [initialRenderComplete, setInitialRenderComplete] = React.useState(false);
@@ -62,48 +55,123 @@ export default function Home() {
                      <legend>What is your linkage to endometriosis?</legend>
                      <div>
                         <input 
-                        type="radio" 
-                        id="family" 
-                        name="linkage" 
-                        value="family"
-                        checked={selectedOption === 'family'}
-                        onChange={handleOptionChange}
+                        type="radio" id="family" name="linkage" value="family"
                         />
                         <label htmlFor="family">A family member / friend / acquaintance is or might be affected from endometriosis.</label>
                     </div>
                     <div>
                         <input 
-                        type="radio" 
-                        id="diagnosis" 
-                        name="linkage" 
-                        value="diagnosis"
-                        checked={selectedOption === 'diagnosis'}
-                        onChange={handleOptionChange}
+                        type="radio" id="diagnosis" name="linkage" value="diagnosis"
                         />
                         <label htmlFor="diagnosis">I have the diagnosis endometriosis.</label>
                     </div>
                     <div>
                         <input 
-                        type="radio" 
-                        id="might_have" 
-                        name="linkage" 
-                        value="might_have"
-                        checked={selectedOption === 'might_have'}
-                        onChange={handleOptionChange}
+                        type="radio" id="might_have" name="linkage" value="might_have"
                         />
                         <label htmlFor="might_have">I think I might have endometriosis.</label>
                     </div>
                     <div>
                         <input 
-                        type="radio" 
-                        id="learn_more" 
-                        name="linkage" 
-                        value="learn_more"
-                        checked={selectedOption === 'learn_more'}
-                        onChange={handleOptionChange}
+                        type="radio" id="learn_more" name="linkage" value="learn_more"
                         />
                         <label htmlFor="learn_more">I personally don’t have a private linkage but would like to learn more about endometriosis.</label>
                     </div>   
+                     <br/>
+                    <legend>For what do you want to use EndoMeet? (Multiple answers possible)</legend>
+                     <div>
+                        <input 
+                        type="radio" id="help" name="usage" value="help"
+                        />
+                        <label htmlFor="help">I want to help others who are suffering from endometriosis by sharing my experiences.</label>
+                    </div>
+                    <div>
+                        <input 
+                        type="radio" id="learn" name="usage" value="learn"
+                        />
+                        <label htmlFor="learn">I am not affected but I want to learn more about endometriosis.</label>
+                    </div>
+                    <div>
+                        <input 
+                        type="radio" id="affected" name="usage" value="affected"
+                        />
+                        <label htmlFor="affected">I am affected and would like to exchange problems / experiences with others in the same position.</label>
+                    </div>
+                    <div>
+                        <input 
+                        type="radio" id="experiences" name="usage" value="experiences"
+                        />
+                        <label htmlFor="experiences">I would like to share my experiences with others who want to learn more about endometriosis.</label>
+                    </div>   
+                    <br/>
+                    <legend>With how many different groups do you want to be matched with?</legend>
+                     <div>
+                        <input 
+                        type="radio" id="one" name="groups" value="one"
+                        />
+                        <label htmlFor="one">Only one other person / one group.</label>
+                    </div>
+                    <div>
+                        <input 
+                        type="radio" id="two" name="groups" value="two"
+                        />
+                        <label htmlFor="two">No more than two groups.</label>
+                    </div>
+                    <div>
+                        <input 
+                        type="radio" id="three" name="groups" value="three"
+                        />
+                        <label htmlFor="three">No more than three groups.</label>
+                    </div>
+                    <div>
+                        <input 
+                        type="radio" id="four" name="groups" value="four"
+                        />
+                        <label htmlFor="four">No more than four groups.</label>
+                    </div>   
+                    <div>
+                        <input 
+                        type="radio" id="limit" name="groups" value="limit"
+                        />
+                        <label htmlFor="limit">I don't have a limit.</label>
+                    </div>   
+                    <br/>
+                    <legend>With how many people do you want to communicate?</legend>
+                     <div>
+                        <input 
+                        type="radio" id="one1" name="people" value="one1"
+                        />
+                        <label htmlFor="one1">Just one other person.</label>
+                    </div>
+                    <div>
+                        <input 
+                        type="radio" id="two2" name="people" value="two2"
+                        />
+                        <label htmlFor="two2">In a group of three.</label>
+                    </div>
+                    <div>
+                        <input 
+                        type="radio" id="three3" name="people" value="three3"
+                        />
+                        <label htmlFor="three3">In a group of four.</label>
+                    </div>
+                    <div>
+                        <input 
+                        type="radio" id="four4" name="people" value="four4"
+                        />
+                        <label htmlFor="four4">In a group of five.</label>
+                    </div>   
+                    <div>
+                        <input 
+                        type="radio" id="preference" name="people" value="preference"
+                        />
+                        <label htmlFor="preference">I don't have a preference.</label>
+                    </div>   
+                    <br/>
+                    <div className={style.form_button}>
+                    <a href="#">Submit</a>
+
+                    </div>
             </form>
         </div>
 
