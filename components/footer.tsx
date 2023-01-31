bc189b12e99279fc8011ea26e9b36f353d26bdec
Dani <layerdani@gmail.com>
2023-01-31 17:34:17 +0100

    Added Imprint to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 function Footer() {
     return (
         <div className={style.legal_content}>
-        <span className="legal-text">&#169; EndoMeet</span> <Link href="#">Impressum</Link>   <Link href="../contact">Contact</Link>   <Link href="#">Data Protection</Link> <Link href="#">Cookies</Link>
+        <span className="legal-text">&#169; EndoMeet</span> <Link href="#">Imprint</Link>   <Link href="../contact">Contact</Link>   <Link href="#">Data Protection</Link> <Link href="#">Cookies</Link>
         </div>
     )
 }
